025c87a27a8329f83a4575aca8a8ae77e4be1e76
Martin Christoph Frank <martin7.frank7@gmail.com>
2024-01-01 13:53:18 +0100

    changed path to note in setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'note=note.main:cli',
+            'note=mycelium.note.main:cli',
         ],
     },
     install_requires=[
