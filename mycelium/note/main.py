0293e3313416aea0a377596b6e8e732240e9652c
Martin Christoph Frank <martin7.frank7@gmail.com>
2024-01-03 20:37:48 +0100

    fixed note to use node_type for repo

--- mycelium/note/main.py
+++ mycelium/note/main.py
@@ -10,37 +10,41 @@ PATH_NOTES = os.getenv("PATH_NOTES", Path.home() / "notes" / "notes")
 EDITOR = os.getenv('EDITOR', 'notepad' if os.name == 'nt' else 'vim')
 FILE_EXTENSION = "md"
 
+def get_repo_notes() -> Repository:
+    """Get the notes repository."""
+    return Repository(node_type=Note)
+
 
 def create_new_note() -> None:
     """Create and open a new note, default action."""
-    repo = Repository()
-    new_note = Note.new(repo)
+    repo = get_repo_notes()
+    new_note = repo.new_node()
     new_note.edit()
 
 
 def list_notes() -> list:
     """List all notes."""
-    repo = Repository()
+    repo = get_repo_notes()
     return repo.nodes
 
 
 def print_note(index: int = -1) -> str:
     """Read a specific note by index."""
-    repo = Repository()
+    repo = get_repo_notes()
     note = Note.from_repository(repo=repo, index=index)
     return note.meta
 
 
 def edit_note(index: int = -1):
     """Edit a specific note by index."""
-    repo = Repository()
+    repo = get_repo_notes()
     note = Note.from_repository(repo=repo, index=index)
     note.edit()
 
 
 def search_notes(term: str):
     """Search for a term in all notes."""
-    repo = Repository()
+    repo = get_repo_notes()
     result = ""
     for index in repo.index_nodes:
         note = Note(repo=repo, index=index)
