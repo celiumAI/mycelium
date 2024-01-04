260d780e2496084ec7f1c3779c094dc64d85cd81
Martin Christoph Frank <martin7.frank7@gmail.com>
2024-01-04 21:35:50 +0100

    making life simpler

---
+++ mycelium/llm.py
@@ -0,0 +1,45 @@
+import json
+import requests
+
+LLM_MODEL = "rephrase"
+
+
+def chat(messages, model=LLM_MODEL):
+    r = requests.post(
+        "http://0.0.0.0:11434/api/chat",
+        json={"model": model, "messages": messages, "stream": True},
+    )
+    r.raise_for_status()
+    output = ""
+
+    for line in r.iter_lines():
+        body = json.loads(line)
+        if "error" in body:
+            raise Exception(body["error"])
+        if body.get("done") is False:
+            message = body.get("message", "")
+            content = message.get("content", "")
+            output += content
+            print(content, end="", flush=True)
+
+        if body.get("done", False):
+            message["content"] = output
+            return message
+
+
+def main():
+    messages = []
+
+    while True:
+        user_input = input("Enter a prompt: ")
+        if not user_input:
+            exit()
+        print()
+        messages.append({"role": "user", "content": user_input})
+        message = chat(messages)
+        messages.append(message)
+        print("\n\n")
+
+
+if __name__ == "__main__":
+    main()
